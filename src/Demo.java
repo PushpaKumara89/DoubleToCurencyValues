3723aab215f0730d0a8f9795b2c5b52ff25f95e1
kpushpakumara89 <kpushpakumarajd@gmail.com>
2021-07-20 11:48:26 +0530

    2nd Comm

--- src/Demo.java
+++ src/Demo.java
@@ -2,5 +2,6 @@ public class Demo {
     public static void main(String args[]){
         double amount = 2000000;
         System.out.println(String.format("%,.2f", amount));  //  2,000,000.00
+        //git Commit here..
     }
 }
